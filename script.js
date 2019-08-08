29b930e68e0bfad19d7a8b5650a99cc5f6093147
mfhan <mf212mf@gmail.com>
2019-08-08 17:14:27 -0400

    add JavaScript logic for database access; add icons for storytelling; streamline navbar

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const kantDos= [
   'Beat Martha Stewart at a Turkey Cook-Off',
   'Fix your washer-dryer (sorry!)',
-  '"Run right into hell and back"',
+  '"Run Right Into Hell And Back"',
   '"Catch a grenade for you"',
   'Sing Cosette\'s part in Les Miserables',
   'Manage the New York Mets\' relief pitching staff',
@@ -13,18 +13,24 @@ const kantDos= [
   'Explain the infield fly rule in baseball',
   'Toss pizza dough in the air',
   'Understand everything immediately',
-  'Beat a 6-year-old at chess'
+  'Beat a 6-year-old at chess',
+  'Make your cat cuddle with you',
+  'Rewrite the ending of Game of Thrones (or maybe I can?)'
 ]
 
 const canDos = [
   'Analyze and solve problems',
   'Use React.js to build fun apps',
-  'Listen to product owners and UX designers',
+  'Actively listen to product owners and UX designers',
   'Communicate with tech teams',
   'Write amazing technical documentation',
-  'Name variables in a memorable way',
+  'Clearly set technical expectations and deliverables',
+  'Write code that\'s clear, concise and scalable',
   'Explain my code in an engaging way',
-  'Build tech communities',
+  'Build and grow tech communities via Meetups',
+  'Speak to any audience with the aim of reaching everyone',
+  'Fully use my journalism background in the service of code',
+  'Make use of my Scrum Master(TM) certification to ensure Agile workflows',
   'Help engineers explain their code',
   'Help non-coders understand what code does',
   'Use an API to populate this portfolio site',
@@ -78,6 +84,123 @@ canDoList.forEach((element) => {
   document.querySelector('.cando').appendChild(li)
   // return element
 })
+
+let funSkill = ''
+function createFunSkill(funCanDos) {
+  funSkill = (funCanDos.splice(Math.floor(Math.random() *deck.length), 1))[0]
+  return funSkill
+}
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myToggler() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
+let myProjectsUrl= 'https://docs.google.com/spreadsheets/d/1JzfzPc5KeR3h1kXCp99eI-UnpKKCUpxx1rDDnS3wFsE/edit#gid=0'
+
+let myNewsUrl = 'https://docs.google.com/spreadsheets/d/1rcUA2TUK1GoSgkCdEzHSnsoO0X8cHwLoARB35rjlQgs/edit#gid=0'
+
+
+// ID COMES FROM THE URL THAT IS IN THE ADDRESS BAR ONCE THE SHEET HAS BEEN CREATED/SHARED
+let projectId = '1JzfzPc5KeR3h1kXCp99eI-UnpKKCUpxx1rDDnS3wFsE'
+
+let newsId = '1rcUA2TUK1GoSgkCdEzHSnsoO0X8cHwLoARB35rjlQgs'
+
+
+// BELOW URL IS HOW GOOGLE ALLOWS US TO ACCESS THE SHARED FILE AS JSON
+let projectSource = `https://spreadsheets.google.com/feeds/list/${projectId}/od6/public/values?alt=json`
+
+let newsSource = `https://spreadsheets.google.com/feeds/list/${newsId}/od6/public/values?alt=json`
+
+// https://spreadsheets.google.com/feeds/list/15PmioBi2dQEkewpqI7MDkDpvcVF0Trw8vmarAQbwoHk/1/public/values?alt=json
+
+fetch(projectSource)
+  .then( res => res.json())
+  .then( data => {
+     console.log('this id data.feed.entry', data.feed.entry)
+     let projects = data.feed.entry.map( d => {
+       return {
+          title: d.gsx$title.$t,
+          image: d.gsx$image.$t,
+          description: d.gsx$description.$t
+       }
+     })
+     console.log('these are my projects', projects)
+     createCards(projects)
+})
+
+
+class Card {
+  constructor(obj) {
+    this.title = obj.title
+    this.image = obj.image
+    this.description = obj.description
+  }
+
+  render() {
+    const col = document.createElement('div')
+    col.classList.add('col','s12','m3');
+
+    const card = document.createElement('div')
+    card.classList.add('card');
+
+    const cardImage = document.createElement('div')
+    cardImage.classList.add('card-image');
+
+    const image  = document.createElement('img')
+    image.setAttribute('src', this.image)
+
+    const cardTitle = document.createElement('span')
+    cardTitle.classList.add('card-title');
+    cardTitle.innerText = this.title
+
+    const cardContent = new CardContent(this.description)
+    console.log('this is cardContent', cardContent)
+    cardImage.appendChild(image)
+   card.appendChild(cardTitle)
+    card.appendChild(cardImage)
+
+    card.appendChild(cardContent.render())
+    col.appendChild(card)
+
+    return col
+  }
+}
+
+class CardContent {
+  constructor(desc) {
+    this.desc = desc
+  }
+  render () {
+    console.log('this is desc', this.desc)
+    const cardContent = document.createElement('div')
+    cardContent.classList.add('card-content');
+
+    const paragraph = document.createElement('p')
+    paragraph.innerText = this.desc
+
+    cardContent.appendChild(paragraph)
+    return cardContent
+  }
+}
+
+function createCards(projects){
+  const projectDiv = document.querySelector('#projects')
+  projects.forEach( obj => {
+    let card = new Card(obj)
+    console.log('this is card', card)
+    projectDiv.appendChild(card.render())
+  })
+
+}
 
 
 //
